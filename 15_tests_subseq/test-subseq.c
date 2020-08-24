53c70184bff444fe3820ecf9925fa1ddc941fae1
Coursera Learner <nobody@nowhere.nul>
2020-08-24 14:30:50 +0000

    Did assignment 15

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -8,27 +8,39 @@ int main(void)
   size_t  a1;
   int d [] = {9, 8, 7, 6};
   a1 = maxSeq(d, 4);
-  if ( a1 != 0)
+  if ( a1 != 1)
     {
       exit(EXIT_FAILURE);
     }
 
   
- int a []= { -25, -20, 5, 9};
- size_t a2 = maxSeq(a, 4);
+ int arr1 []= { -25, -4, 5, 9};
+ size_t a2 = maxSeq(arr1, 4);
   if ( a2 != 4) {
-    printf("Mi\n");
-    exit( EXIT_FAILURE);}
+     exit( EXIT_FAILURE);}
 
-  int b []= {0};
-  size_t a3 = maxSeq(b, 1);
+  int arr2 []= {0};
+  size_t a3 = maxSeq(arr2, 1);
   if ( a3 != 1) {
-    printf("ho\n");
     exit(EXIT_FAILURE);}
 
-  int c []= {2 , 3 , 4 , 3, 5, 7, 9};
-  size_t a4 = maxSeq(c, 8);
-  if(a4 != 4) { printf("lool\n"); exit(EXIT_FAILURE);}
+  int arr3 []= {2 , 3 , 4 , 3, 5, 7, 9, 5};
+  size_t a4 = maxSeq(arr3, 9);
+  if(a4 != 4) { exit(EXIT_FAILURE);}
+
+  int arr4 [] = {-1, -2, 0, 1, 2};
+  if (maxSeq(arr4, 5) != 4) {exit(EXIT_FAILURE);}
+
+  int arr5 [] = {-9, -8, -7, -1};
+  if (maxSeq(arr5, 4) != 4) {exit(EXIT_FAILURE);}
+
+  int arr6 [] = {};
+  if (maxSeq(arr6, 0) != 0) {exit(EXIT_FAILURE);}
+
+  int arr7 [] = {1,2,2,3,3,6,7,8};
+  if (maxSeq(arr7, 8) != 4) {exit(EXIT_FAILURE);}
+  int arr8 [] = {1, 1, 1};
+  if (maxSeq(arr8, 3) != 1) {exit(EXIT_FAILURE);}
   
 
   return EXIT_SUCCESS;
