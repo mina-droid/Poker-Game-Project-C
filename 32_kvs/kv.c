2f828dfe4739af19d5a137fd0fdcce1d23cc68c8
Coursera Learner <nobody@nowhere.nul>
2020-09-28 00:03:46 +0000

    done

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,84 +3,78 @@
 #include <string.h>
 #include "kv.h"
 
-char *  stripNewline(char * str)  {
-  char * p = strchr(str, '=');
-  char * s = strchr(str, '\n');
-  if ( s != NULL)
-    {
-      *s = '\0';
-    }
-  size_t len = strlen(str);
-  *p = '\0';
-  p++;
-  p[len ] = '\0';
-  return p;
-  
+kvpair_t* splitPairs(char* str){
+
+  char* value1=strchr(str,'=');
+  *value1='\0';
+  value1 ++;
+  char* n=strchr(value1,'\n');
+  if(n != NULL) *n='\0';
+
+  kvpair_t* pair = malloc((size_t) sizeof(*pair));
+  pair ->key=str;
+  pair ->value=value1;
+  return pair;
 }
 
 kvarray_t * readKVs(const char * fname) {
-  FILE * f = fopen(fname,"r");
-  if ( f == NULL)
-    {
-      fprintf(stderr,"could not open file!");
-      exit (EXIT_FAILURE);
-    }
-  size_t sz = 0;
-  char * str = NULL;
-  kvarray_t * ans = malloc (sizeof(*ans));
-  ans -> kvarr = malloc (sizeof(*ans -> kvarr));
-  ans -> kvarrsize = 0;
-  while (getline(&str, &sz, f) >= 0)
-    {
-      if ( str[0] =='\n')
-	{
-	  continue;
-	}
-      ans -> kvarr = realloc (ans -> kvarr, ((ans -> kvarrsize + 1) * sizeof(*ans -> kvarr)));
-      ans -> kvarr[ans ->kvarrsize] = malloc (sizeof(*ans -> kvarr[ans -> kvarrsize]));
-      ans -> kvarr[ans -> kvarrsize] -> value =  stripNewline(str);
-      ans -> kvarr[ans -> kvarrsize] -> key =str;
-      str = NULL;
-      ans -> kvarrsize++;
-    }
-  free(str);
-  if (fclose(f) != 0)
-    {
-      fprintf(stderr,"could not close file!");
-    }
-  return ans;
-  
-  //WRITE ME
+  //open the file
+  FILE * f = fopen(fname, "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return NULL;
+  }
+
+  //read the lines
+  char* line= NULL;
+  size_t sz=0;
+  kvpair_t* curr_pair = NULL;
+
+  kvarray_t* kv_array= malloc((size_t)(sizeof(*kv_array)));
+  kv_array->size = 0;
+  kv_array->arr=NULL;
+
+  while (getline(&line,&sz, f) >= 0) {
+    curr_pair=splitPairs(line);
+    kv_array->arr=realloc(kv_array->arr,(kv_array->size+1)*sizeof(*kv_array->arr));
+    kv_array->arr[kv_array->size]=curr_pair;
+    kv_array->size ++;
+    line=NULL;
+  }
+  free(line);
+
+  int result=fclose(f);
+  if (result !=0)return NULL;
+  return kv_array;
 }
 
-
 void freeKVs(kvarray_t * pairs) {
-  for ( int i = 0; i < pairs -> kvarrsize; i++)
-    {
-      free(pairs -> kvarr[i]-> key);
-      free(pairs -> kvarr[i]);
-    }
-  free(pairs -> kvarr);
-  free(pairs);
-  
   //WRITE ME
+  for(int i=0 ;i<pairs->size;i++){
+
+    free(pairs->arr[i]->key);
+    free(pairs->arr[i]);
+  }
+  free(pairs->arr);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
-  for ( int i = 0; i < pairs -> kvarrsize; i++){
-    printf("key = '%s' value = '%s'\n", pairs ->kvarr[i]->key, pairs -> kvarr[i] -> value);
+
+  kvpair_t* curr_pair;
+  for(int i=0;i<pairs->size;i++){
+    curr_pair=pairs->arr[i];
+    printf("key = '%s' value = '%s'\n" ,curr_pair->key, curr_pair->value);
   }
   //WRITE ME
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
-  for ( int i = 0; i < pairs -> kvarrsize; i++)
-    {
-      if ( strcmp(pairs -> kvarr[i] -> key, key) == 0)
-	{
-	  return pairs -> kvarr[i] -> value;
-	}
-    }
-  return NULL;
+  kvpair_t* curr_pair;
+  for(int i=0;i<pairs->size;i++){
+    curr_pair=pairs->arr[i];
+    if (! strcmp(curr_pair->key,key)) return  curr_pair->value;
+  }
   //WRITE ME
+  return NULL;
 }
--- 32_kvs/kv.h
+++ 32_kvs/kv.h
@@ -3,18 +3,16 @@
 
 
 struct _kvpair_t {
-  char * key;
-  char * value;
   //DEFINE ME!
-
+  char* key;
+  char* value;
 };
 typedef struct _kvpair_t kvpair_t;
 
 struct _kvarray_t {
-  kvpair_t ** kvarr;
-  int kvarrsize;
   //DEFINE ME!
-
+  kvpair_t** arr;
+  int size;
 
 };
 typedef struct _kvarray_t kvarray_t;
