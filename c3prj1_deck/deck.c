2137cdbcb448add9faa525997c94d7421e861adb
Coursera Learner <nobody@nowhere.nul>
2020-09-29 21:41:39 +0000

    c4p1

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,6 +2,89 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
+
+void add_card_to(deck_t * deck, card_t c)
+{
+  deck -> cards = realloc ( deck -> cards, deck -> n_cards * sizeof(*deck -> cards));
+  deck -> cards[n_cards] -> value = c -> value;
+  deck -> cards[n_cards] -> suit = c -> suit;
+    
+}
+
+
+card_t * add_empty_card(deck_t * deck)
+{
+  card_t * ans;
+  ans -> value = 0;
+  ans -> suit = 0;
+  deck -> cards[n_cards] -> value = ans -> value;
+  deck ->cards[n_cards]-> suit = ans -> suit;
+  return ans;
+  
+}
+
+
+deck_t * make_deck_exclude(deck_t * excluded_cards)
+{
+  deck_t * ans = malloc (sizeof(*ans));
+  ans -> n_cards = 52; 
+  ans -> cards = malloc ((ans -> n_cards) * (sizeof(*ans-> cards)));
+  for ( unsigned  i = 0; i < ans -> n_cards; i++)
+    {
+      ans -> cards[i] = card_from_num(i);
+    }
+  for ( unsigned i = 0; i < excluded_cards -> n_cards; i++)
+    {
+      int n =  deck_contains(ans, excluded_cards -> cards[i]);
+      if ( n == 1)
+	{
+	  for ( int j = 0; j < 52; j++)
+	    {
+	      if ( ans -> cards[j] -> value == excluded_cards -> cards[i] -> value)
+		{
+		  if ( ans -> cards[j] -> suit == excluded_cards -> cards[i] -> suit)
+		    {
+		      ans -> cards[j] = NULL;
+		      ans -> n_cards--;
+		    }
+		}
+	    }
+	}
+    }
+  return ans;
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands)
+{
+  deck_t * temp = malloc ( sizeof(*temp));
+  temp -> n_cards = 0;
+  temp -> cards = malloc ( (temp -> n_cards + 1) * ( sizeof(*temp -> cards)));
+  for ( int i = 0; i < n_hands; i++)
+    {
+      for ( int j = 0; j < hands[i] -> n_cards; j++)
+	{
+	  temp -> n_cards++;
+	  add_card_to(temp, hands[i]->cards[j]);
+	}
+    }
+  deck_t * ans = make_deck_exclude(temp);
+  free_deck(ans);
+  free_deck(temp);
+  return ans;
+  
+}
+
+void free_deck(deck_t * deck)
+{
+  for ( int i = 0; i < deck -> n_cards; i++)
+    {
+      free(deck -> cards[i]);
+    }
+  free(deck -> cards);
+  free(deck);
+  
+}
+
 void print_hand(deck_t * hand){
   size_t n = hand->n_cards;
   card_t temp;
